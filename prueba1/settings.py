8c0a164c1e6d180d897b88097d57d238d69f2029
Rodrigo Zetina Cervantes <zetinacervantes7@gmail.com>
2025-08-06 13:15:12 -0600

    Update settings.py

--- prueba1/settings.py
+++ prueba1/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'usuariosdb',
         'USER': 'postgres',
-        'PASSWORD': 'roycomejamon',
+        'PASSWORD': 'xxxxxx',
         'HOST': 'localhost',
         'PORT': '5432',
     }
